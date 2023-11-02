cc48fb31bc11bd06b108172624bcec6521cdb921
Lucas <flucasyoel@gmail.com>
2023-11-02 20:54:18 -0300

    Create Proyect

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Componente1 from "./components/componente1";
+
+function App() {
+  return (
+    <>
+      <div>Compoente 1 </div>
+      <Componente1></Componente1>
+      <div>Compoente 2 </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Componente1.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Componente1 = () => {
+  return <button>click</button>;
+};
+
+export default Componente1;
