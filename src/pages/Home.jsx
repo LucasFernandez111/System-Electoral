0c555f4d86400ef3899f8c4e181f8588872ea821
Lukitas <flucasyoel@outlook.es>
2023-11-03 14:07:27 -0300

    Add footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,16 +9,14 @@ const user = {
     "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 };
 const navigation = [
-  { name: "Dashboard", href: "#", current: true },
-  { name: "Team", href: "#", current: false },
-  { name: "Projects", href: "#", current: false },
-  { name: "Calendar", href: "#", current: false },
-  { name: "Reports", href: "#", current: false },
+  { name: "Inicio", href: "#", current: true },
+  { name: "Votar", href: "#", current: false },
+  { name: "Seguimiento y Resultado", href: "#", current: false },
 ];
 const userNavigation = [
-  { name: "Your Profile", href: "#" },
-  { name: "Settings", href: "#" },
-  { name: "Sign out", href: "#" },
+  { name: "Tú Perfil", href: "#" },
+
+  { name: "Cerrar Sesion", href: "#" },
 ];
 
 function classNames(...classes) {
@@ -64,14 +62,15 @@ export default function Example() {
                   </div>
                   <div className="hidden md:block">
                     <div className="ml-4 flex items-center md:ml-6">
-                      <button
+                      {/* Tinker Button */}
+                      {/* <button
                         type="button"
                         className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                       >
                         <span className="absolute -inset-1.5" />
                         <span className="sr-only">View notifications</span>
                         <BellIcon className="h-6 w-6" aria-hidden="true" />
-                      </button>
+                      </button> */}
 
                       {/* Profile dropdown */}
                       <Menu as="div" className="relative ml-3">
@@ -201,10 +200,8 @@ export default function Example() {
         </Disclosure>
 
         <header className="bg-white shadow">
-          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-              Dashboard
-            </h1>
+          <div className="justify-center flex  px-4 py-6 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold  text-gray-900">FISCAL</h1>
           </div>
         </header>
         <main>
