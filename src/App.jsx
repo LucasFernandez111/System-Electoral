d04d022177eff101cfcd7643e7ce1fe0137e8d82
Lukitas <flucasyoel@outlook.es>
2023-11-02 23:57:42 -0300

    Remove

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,5 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import Componente1 from "./components/componente1";
-
 function App() {
-  return (
-    <>
-      <div>Compoente 1 </div>
-      <h1>gmiemg</h1>
-      <Componente1></Componente1>
-      <div>Compoente 2 </div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
--- src/components/Componente1.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Componente1 = () => {
-  return <button>click</button>;
-};
-
-export default Componente1;
