31cc5f2a98d3a49300b7d82924bdcd57aeaa4320
Lukitas <flucasyoel@outlook.es>
2023-11-02 23:56:18 -0300

    Add Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 import Componente1 from "./components/componente1";
 
 function App() {
   return (
     <>
       <div>Compoente 1 </div>
+      <h1>gmiemg</h1>
       <Componente1></Componente1>
       <div>Compoente 2 </div>
     </>
