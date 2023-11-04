ba3fbf54720f46daf802caf7fbbc9aa8253bd6a5
yasminsitaa <rallas674@gmail.com>
2023-11-04 00:00:53 -0300

    add Partidos, Login, Footer compt, and changes in Register

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 export default function Login() {
   return (
     <>
-      <div className="flex min-h-full flex-1  flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
             className="mx-auto h-10 w-auto"
@@ -9,45 +9,14 @@ export default function Login() {
             alt="Your Company"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight ">
-            Registre su cuenta
+            Iniciar Sesión
           </h2>
         </div>
-
-        <label
-          for="Toggle3"
-          className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm p-2 rounded-md cursor-pointer dark:text-gray-800"
-        >
-          <input id="Toggle3" type="checkbox" className="hidden peer" />
-          <span className="px-4 py-2 rounded-l-md dark:bg-indigo-600  text-white  peer-checked:dark:bg-gray-300  peer-checked:dark:text-gray-800">
-            Fiscal
-          </span>
-          <span className="px-4 py-2 rounded-r-md dark:bg-gray-300 peer-checked:dark:bg-indigo-600 peer-checked:dark:text-white">
-            Votante
-          </span>
-        </label>
+        
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6" action="#" method="POST">
-            {/* Nombre de usuario */}
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Nombre de Usuario
-              </label>
-              <div className="mt-2">
-                <input
-                  id="user"
-                  name="user"
-                  type="user"
-                  autoComplete="user"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
+           
             {/* Email */}
             <div>
               <label
@@ -63,7 +32,7 @@ export default function Login() {
                   type="email"
                   autoComplete="email"
                   required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block px-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
@@ -85,7 +54,7 @@ export default function Login() {
                   type="password"
                   autoComplete="current-password"
                   required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block px-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
@@ -95,76 +64,12 @@ export default function Login() {
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Registrar
+                Ingresar
               </button>
             </div>
 
-            <div>
-              <p className="text-center text-xl font-medium leading-6 text-gray-900">
-                ó
-              </p>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Iniciar Sesión
-              </button>
-            </div>
           </form>
         </div>
-
-        <footer>
-          <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-            <div class="sm:flex sm:items-center sm:justify-between">
-              <a
-                href="https://flowbite.com/"
-                class="flex items-center mb-4 sm:mb-0"
-              >
-                <img
-                  src="https://flowbite.com/docs/images/logo.svg"
-                  class="h-8 mr-3"
-                  alt="Flowbite Logo"
-                />
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                  Flowbite
-                </span>
-              </a>
-              <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-                <li>
-                  <a href="#" class="mr-4 hover:underline md:mr-6 ">
-                    About
-                  </a>
-                </li>
-                <li>
-                  <a href="#" class="mr-4 hover:underline md:mr-6">
-                    Privacy Policy
-                  </a>
-                </li>
-                <li>
-                  <a href="#" class="mr-4 hover:underline md:mr-6 ">
-                    Licensing
-                  </a>
-                </li>
-                <li>
-                  <a href="#" class="hover:underline">
-                    Contact
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-            <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-              © 2023{" "}
-              <a href="https://flowbite.com/" class="hover:underline">
-                Flowbite™
-              </a>
-              . All Rights Reserved.
-            </span>
-          </div>
-        </footer>
       </div>
     </>
   );
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "../pages/Login";
 import DashBoard from "../pages/DashBoard";
+import Register from "../pages/Register";
+import Partidos from "../pages/Partidos";
 
 import CardPost from "../components/CardPost";
 const Routers = () => {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/partidos-selection" element={<Partidos />} />
+       <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/inicio" element={<DashBoard />} />
       <Route path="/prueba" element={<CardPost></CardPost>} />
     </Routes>
