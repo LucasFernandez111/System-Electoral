220b0b22cc62c0218bfa2c67e5fab27f739774d8
yasminsitaa <rallas674@gmail.com>
2023-11-05 01:21:54 -0300

    add votation and partidos comp

--- src/pages/Partidos.jsx
+++ src/pages/Partidos.jsx
@@ -1,43 +1,46 @@
 export default function Partidos() {
-    return (
-      <>
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img
-              className="mx-auto h-10 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt="Your Company"
-            />
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight ">
-              ¿Qué partido desea fiscalizar?
-            </h2>
-          </div>
-          
-  
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6" action="#" method="POST">
-             
-              <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Unión por la Patria
-                </button>
-              </div>
-
-              <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  La Libertad Avanza
-                </button>
-              </div>
-  
-            </form>
-          </div>
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          />
+          <h2 className="mt-10  mb-8 text-center text-2xl font-bold leading-9 tracking-tight ">
+            ¿Qué partido desea fiscalizar?
+          </h2>
         </div>
-      </>
-    );
-  }
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-8" action="#" method="POST">
+            <div className="mb-8">
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md px-3 py-1.5 text-lg font-semibold leading-6 text-white shadow-sm  group bg-gradient-to-r from-violet-950 via-purple-700 to-purple-950 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800  "
+              >
+                <span class="flex w-full justify-center text-gray-900 px-5 py-1.5 transition-all ease-in duration-75 bg-white dark:bg-gray-300 rounded-md group-hover:bg-opacity-0  hover:text-white">
+                  La Libertad Avanza
+                </span>
+              </button>
+            </div>
+
+
+            <div>
+            <button
+                type="submit"
+                className="flex w-full justify-center rounded-md px-3 py-1.5 text-lg font-semibold leading-6  shadow-sm  group bg-gradient-to-r from-sky-400 via-amber-400 to-sky-300 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800  "
+              >
+                <span class="flex w-full justify-center  text-gray-900  px-3 py-1.5 transition-all ease-in duration-75 bg-white dark:bg-gray-300 rounded-md group-hover:bg-opacity-0 ">
+                  Unión Por La Patria
+                </span>
+              </button>
+            </div>
+
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -4,11 +4,13 @@ import Login from "../pages/Login";
 import DashBoard from "../pages/DashBoard";
 import Register from "../pages/Register";
 import Partidos from "../pages/Partidos";
+import Votar from "../components/Votation";
 
 import CardPost from "../components/CardPost";
 const Routers = () => {
   return (
     <Routes>
+       <Route path="/votar" element={<Votar />} />
       <Route path="/partidos-selection" element={<Partidos />} />
        <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
