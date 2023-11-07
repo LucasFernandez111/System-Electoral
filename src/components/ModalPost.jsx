84c78a7a38a1e7f073b869a578c1075d111374c0
Lucas <flucasyoel@gmail.com>
2023-11-06 21:52:37 -0300

    Style Create Post

--- src/components/ModalPost.jsx
+++ src/components/ModalPost.jsx
@@ -1,19 +1,46 @@
 import React from "react";
+import { IoMdPhotos } from "react-icons/io";
+import { IconButton } from "@material-tailwind/react";
 
 const ModalPost = ({ isModalOpen, setIsModalOpen }) => {
   return (
     <>
       {isModalOpen && (
-        <div className="fixed inset-0  bg-gray-700 bg-opacity-75 transition-opacity  flex justify-center items-center">
-          <div className="bg-white rounded-xl   flex justify-center  h-2/5 w-2/5 items-center">
-            <button
-              onClick={() => {
-                setIsModalOpen(false);
-              }}
-              className="py-3 px-4 bg-blue-500 rounded-lg text-white font-semibold"
-            >
-              close
-            </button>
+        <div className="fixed inset-0  bg-gray-700 bg-opacity-75 transition-opacity   flex justify-center  items-center">
+          <div className="bg-white rounded-xl transition-all   flex justify-center flex-col w-px-500 px-12 py-11 items-center gap-10">
+            <textarea
+              placeholder="Escriba su post..."
+              className="border-2 border-gray-400 font-semibold rounded-lg p-4"
+              name=""
+              id=""
+              cols="90"
+              rows="8"
+            ></textarea>
+
+            <div className="flex flex-row  justify-between items-center gap-96 ">
+              <IconButton
+                variant="text"
+                className="items-center flex justify-center"
+                color="blue-gray"
+                size="sm"
+              >
+                <IoMdPhotos size={30}></IoMdPhotos>
+              </IconButton>
+              <div className="flex justify-between gap-4">
+                <button
+                  onClick={() => setIsModalOpen(false)}
+                  className="border border-gray rounded-lg p-2  font-semibold"
+                >
+                  Cancelar
+                </button>
+                <button
+                  onClick={() => setIsModalOpen(false)}
+                  className=" bg-gray-900 text-white p-2 rounded-lg font-semibold"
+                >
+                  Publicar Post
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       )}
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -5,26 +5,18 @@ import DashBoard from "../Views/DashBoard";
 import Register from "../Views/Register";
 import Partidos from "../Views/Partidos";
 import Votar from "../components/Votation";
-
-
 import Results from "../components/Results";
-
-
 
 const Routers = () => {
   return (
     <Routes>
-
-     
       <Route path="/votar" element={<Votar />} />
       <Route path="/partidos-selection" element={<Partidos />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
       <Route path="/inicio" element={<DashBoard />} />
 
-      <Route path="/prueba" element={<Results/>} />
-
-
+      <Route path="/prueba" element={<Results />} />
     </Routes>
   );
 };
