5066a5da310fa048be0e86d380874144fd1783a5
yasminsitaa <rallas674@gmail.com>
2023-11-12 19:43:47 -0300

    add logo

--- src/Views/DashBoard.jsx
+++ src/Views/DashBoard.jsx
@@ -110,8 +110,8 @@ const DashBoard = () => {
                   <div className="flex items-center">
                     <div className="flex-shrink-0">
                       <img
-                        className="h-8 w-8"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                        className="h-16 w-16"
+                        src="./imagenes/votarg.png.png"
                         alt="Your Company"
                       />
                     </div>
