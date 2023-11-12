c7a6337de945164155bdbe10a44b37845a8ec177
yasminsitaa <rallas674@gmail.com>
2023-11-12 19:03:23 -0300

    changes pfp

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -7,14 +7,14 @@ export default function Profile({ ProfileOpen, setProfile }) {
         <div className="fixed inset-0  bg-gray-700 bg-opacity-75 transition-opacity   flex justify-center  items-center ">
           <div className="flex flex-col justify-center max-w-xs p-10 shadow-md rounded-xl sm:px-12 dark:bg-gray-800  dark:text-gray-100 mt-4 ml-4 relative">
             <img
-              src="https://www.gavinjowitt.com/wp-content/uploads/Corporate-Headshots_Gavin-Jowitt-039.jpg"
+              src="https://cdn-icons-png.flaticon.com/512/5709/5709782.png"
               alt=""
               className="w-32 h-32 mx-auto rounded-full dark:bg-gray-500 aspect-square object-cover"
             />
             <div className="space-y-4 text-center divide-y divide-gray-700">
               <div className="my-2 space-y-1">
                 <h2 className="text-xl font-semibold sm:text-2xl">
-                  Leroy Jenkins
+                  Mario Dartinez
                 </h2>
                 <p className="px-5 text-sm sm:text-base dark:text-gray-400 italic">
                   FISCAL
