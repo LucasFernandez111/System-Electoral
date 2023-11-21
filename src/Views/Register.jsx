f0312e92ba01f3754c6c1c79d150768ff94df3d4
Lukitas <flucasyoel@outlook.es>
2023-11-21 00:41:34 -0300

    -

--- src/Views/Register.jsx
+++ src/Views/Register.jsx
@@ -73,7 +73,7 @@ export default function Register() {
   return (
     <>
       <AlertError
-        text={"Usuario ya registrado..."}
+        text={"Correo ya registrado..."}
         setError={setError}
         error={error}
       />
